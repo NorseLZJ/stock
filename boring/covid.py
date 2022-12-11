cf292c2c397fb540a99765e0d1324ef1a6b5030b
liuzijian <18841685054@163.com>
2022-12-11 16:18:53 +0800

    一点pandas的技巧

--- boring/covid.py
+++ boring/covid.py
@@ -5,11 +5,15 @@ import pandas as pd
 pd.set_option("display.max_columns", None)
 pd.set_option("display.width", 500)
 
+covid_csv = "covid.csv"
+
 if __name__ == "__main__":
-    if os.path.exists("covid.xlsx") is False:
+    if os.path.exists(covid_csv) is False:
         covid_19_risk_area_df = ak.covid_19_risk_area(symbol="高风险等级地区")
-        covid_19_risk_area_df.to_excel("covid.xlsx", index=False)
-    covid_df = pd.read_excel("covid.xlsx")
+        covid_19_risk_area_df.to_csv(covid_csv, index=False)
+    covid_df = pd.read_csv(covid_csv)
+    df_xa = covid_df[(covid_df["city"] == "西安市")]
     df = covid_df[(covid_df["city"] == "西安市") & (covid_df["county"] == "未央区")]
-    print(df)
-    print(df.loc[:, ["communitys"]])
+    print("西安高风险数量:", len(df_xa))
+    print(df.loc[:, ["communitys"]].to_string(index=False))
+    # print(df.to_string(index=False))
